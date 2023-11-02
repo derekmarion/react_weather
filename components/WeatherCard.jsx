a03442e53b368d01f22a8d4d27388a3606029ebe
Derek Marion <derek.j.marion@gmail.com>
2023-11-02 14:42:28 -0700

    Add cloudy and rainy animations to WeatherCard

--- components/WeatherCard.jsx
+++ components/WeatherCard.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import * as images from '../src/images';
 
 const WeatherCard = ({ data }) => {
-    const lowerForecast = data.forecast.toLowerCase();
+    const lowerForecast = data.forecast.toLowerCase(); //additional values found at https://developer.accuweather.com/weather-icons
+    
+    const forecastImages = { //object to map forecast value images
+        sunny: images.sunny,
+        rain: images.rainy,
+        cloudy: images.cloudy,
+    };
+
     return (
         <div className="bg-white shadow-md rounded-md p-4 my-4 text-center">
-        Today's forecast in {data.city} is {lowerForecast}. {lowerForecast === "sunny" ? <img src={images.sunny} class='mx-auto' /> : null}
+        Today's forecast in {data.city} is {lowerForecast}. 
+        {forecastImages[lowerForecast] && <img src={forecastImages[lowerForecast]} class='mx-auto' />}
         </div>
     );
 };
--- src/images/index.js
+++ src/images/index.js
@@ -1,3 +1,5 @@
 import sunny from './day.svg';
+import rainy from './rainy-7.svg';
+import cloudy from './cloudy.svg'
 
-export {sunny};
+export {sunny, rainy, cloudy};
